7932cbfc72fbed3ef7dd427893ca99c5cc51c0e7
Leong Teng Man <41246170+taimoon@users.noreply.github.com>
2023-01-13 22:49:11 +0800

    update

--- makenote.py
+++ makenote.py
@@ -2,9 +2,19 @@ import os
 files = [
     "intro.ipynb",
     "EDA.ipynb",
-    "preprocess and model.ipynb"
+    "preprocess and model.ipynb",
+    "final.ipynb"
 ]
-output_name = "final.ipynb"
+quote = lambda f : f'"{f}"'
 
+output_name = 'ignore_final.ipynb'
+if output_name is None:
+    output_name = "ignore_" + "_".join([os.path.splitext(nm)[0] for nm in files]) + ".ipynb"
+
+    output_name = quote(output_name)
+
+
+files = [quote(f) for f in files]
 cmd = "nbmerge " + " ".join(files) + " -o " + output_name
 os.system(cmd)
+print(output_name)
